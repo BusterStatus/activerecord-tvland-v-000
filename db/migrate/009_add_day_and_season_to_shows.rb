32c4c330fee2dc19876be0377779f7bca674d7da
matthew.serre <matthew.serre@gmail.com>
2019-05-25 20:25:08 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_day_and_season_to_shows.rb
+++ db/migrate/009_add_day_and_season_to_shows.rb
@@ -1,6 +1,6 @@
 class AddDayAndSeasonToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :day, :string
-    add_column :shows, :seasonb, :string
+    add_column :shows, :season, :string
   end
 end
